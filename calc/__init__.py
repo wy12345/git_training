8a224b77684dccd8b3ea904394bcc0e0b9f30f99
Salil Vanvari <salilvanvari@berkeley.edu>
2016-09-28 12:54:20 -0700

    Fixed Functionality of pow.

--- calc/__init__.py
+++ calc/__init__.py
@@ -18,7 +18,18 @@ def mod(a, b):
     return -1
 
 def pow(a, b):
-    return a*b
+    """
+    Returns a to the power of b.
+
+    Parameters
+    ---------------
+    a: int or float
+        The base
+    b: int or float
+        The power
+
+    """
+    return a**b
 
 def log(a, base=10):
     return np.log(a)
--- calc/tests/test_calc.py
+++ calc/tests/test_calc.py
@@ -1,5 +1,10 @@
 from calc import add
+from calc import pow
 
 def test_add():
     assert add(1, 2) == 3
 
+def test_pow():
+    assert pow(1,2) == 1
+    assert pow(2,2) == 4
+    assert pow(10,0) == 1
